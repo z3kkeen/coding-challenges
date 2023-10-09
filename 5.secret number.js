1472c1845a580960f6779d0a3e9c040cc7173a31
z3keen <alma@henricson.fi>
2023-10-09 10:30:07 +0300

    rename files + nr. 5

---
+++ 5.secret number.js
@@ -0,0 +1,30 @@
+// - gen random number 1-100
+// - compare user input to randomIndex
+//   ---> if input > index { log('number is lower than' + userInput)}
+//        else { log('number is lower than' + userInput)}
+
+import readline from "read-console-input"
+
+let unknown = true;
+
+function secretNumber() {
+    const secretNumber = Math.floor(Math.random() * (100 - 1) + 1) ;
+
+    while(unknown) {
+        let userInput = JSON.parse(readline("guess the secret number: "));
+
+        if(secretNumber == userInput) {
+            unknown = false
+            console.log('good job! you guessed the secret number !! ^^');
+            console.log('the secret number was indeed ' + secretNumber);
+        }
+
+        if(userInput > secretNumber) {
+            console.log('number is lower than ' + userInput);
+        } else if (userInput < secretNumber) {
+            console.log('number is higher than ' + userInput);
+        }
+    }
+}
+
+secretNumber()
