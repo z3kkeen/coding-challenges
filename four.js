0d374d831195505f124d2277cd635de20876141e
z3keen <alma@henricson.fi>
2023-10-09 10:06:06 +0300

    fifth ex.

---
+++ four.js
@@ -0,0 +1,20 @@
+// - ask for five numbers
+// - add all numbers together then divide by 5
+// - log finale number
+
+import readline from "read-console-input"
+
+let totalCount = 0;
+
+function neverBackDownNeverWHAT() {
+    for(let i = 0; i < 5; i++) {
+       
+        let number = JSON.parse(readline("enter number " + [i] + "/5: "));
+        totalCount = totalCount + number;
+    };
+  
+    const average = totalCount / 5;
+    console.log(average);
+};
+
+neverBackDownNeverWHAT()
