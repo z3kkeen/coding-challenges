16985c56d900a8565987e783659d358a404c0ac3
z3keen <alma@henricson.fi>
2023-10-10 13:28:19 +0300

    ex. 10 + bonus

---
+++ 10.factors.js
@@ -0,0 +1,28 @@
+import readline from "read-console-input";
+
+let factors = [];
+let backwardsFactors = [];
+
+function factorChecker() {
+    let userInput = JSON.parse(readline("give me a number: "));
+
+    for(let i = 0; i < userInput; i++) {
+        if(userInput / i === parseInt(userInput / i)) {
+
+            factors.push(i + " ")
+        };
+    };
+    console.log("factors: " + factors);
+    
+    backwardsFactors.push(userInput)
+
+    for(let i = 0; i < factors.length; i++) {
+
+        let backwardsCurrent = factors.length - i - 1;
+        backwardsFactors.push(factors[backwardsCurrent])
+
+        console.log(factors[i] + " x " + backwardsFactors[i] + " = " + (factors[i] * backwardsFactors[i]));
+    };
+};
+
+factorChecker();
