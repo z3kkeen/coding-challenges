7b82744e8aaebfd98d28187f0c8c0fbafec8cc35
z3keen <alma@henricson.fi>
2023-10-23 09:08:00 +0300

    removed console.log

--- 9.prime-checker.js
+++ 9.prime-checker.js
@@ -4,7 +4,6 @@ function primeChecker() {
     let userInput = Number(readline("enter a number for prime-number check: "));
 
     for(let i = 2; i < userInput; i++) {
-        console.log(i);
         if(userInput % i == 0) {
             console.log(userInput + " is not a prime number.");
             return;
