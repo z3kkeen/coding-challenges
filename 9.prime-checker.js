64a5b0f59233be05a41bb105bfee7c63cd11653a
z3keen <alma@henricson.fi>
2023-10-23 09:09:08 +0300

    final

--- 9.prime-checker.js
+++ 9.prime-checker.js
@@ -3,7 +3,7 @@ import readline from "read-console-input";
 function primeChecker() {
     let userInput = Number(readline("enter a number for prime-number check: "));
 
-    for(let i = 2; i < userInput; i++) {
+    for(let i = 2; i < userInput/2; i++) {
         if(userInput % i == 0) {
             console.log(userInput + " is not a prime number.");
             return;
